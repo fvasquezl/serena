ea732d66335f666063c2eb90ab32fe7005542bb3
Faustino Vasquez <vasquez.limon@gmail.com>
2021-06-22 13:30:40 -0700

    Users CRUD index,create,update

--- app/Http/Livewire/IndexUsers.php
+++ app/Http/Livewire/IndexUsers.php
@@ -3,16 +3,183 @@
 namespace App\Http\Livewire;
 
 use App\Models\User;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class IndexUsers extends Component
 {
+    use WithPagination;
 
+    public $modalFormVisible = false;
+    public $modelId;
+    public $name;
+    public $email;
+    public $password;
+    public $password_confirmation;
+
+    /**
+     * rules
+     *
+     * @return void
+     */
+    public function rules()
+    {
+        $rules =  [
+            'name' => 'required',
+            'email' => ['required', Rule::unique('users', 'email')->ignore($this->modelId)],
+        ];
+
+        if ($this->password || $this->modelId == null) {
+            $rules['password'] = ['confirmed', 'min:6'];
+        }
+
+        return $rules;
+    }
+
+    /**
+     * The livewire mount function
+     *
+     * @return void
+     */
+    public function mount()
+    {   //Resets the pagination after reloading the page
+        $this->resetPage();
+    }
+
+    /**
+     * the create function
+     *
+     * @return void
+     */
+    public function create()
+    {
+        $this->validate();
+        User::create($this->modelData());
+        $this->modalFormVisible = false;
+        $this->resetVars();
+    }
+
+    /**
+     * The read function
+     *
+     * @return void
+     */
     public function read()
     {
         return User::paginate(5);
     }
 
+
+
+    /**
+     * the update function
+     *
+     * @return void
+     */
+    function update()
+    {
+        $this->validate();
+        User::find($this->modelId)->update($this->modelData());
+        $this->modalFormVisible = false;
+    }
+
+    /**
+     * the delete function
+     *
+     * @return void
+     */
+    function delete()
+    {
+
+        User::destroy($this->modelId);
+        //  $this->modalConfirmDeleteVisible = false;
+        $this->resetPage();
+    }
+
+
+    /**
+     * Shows the form modal
+     * of the create function
+     *
+     * @return void
+     */
+    public function CreateShowModal()
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modalFormVisible = true;
+    }
+
+    /**
+     * Show s the form modal
+     *
+     * @param  mixed $id
+     * @return void
+     */
+    public function updateShowModal($id)
+    {
+        $this->resetValidation();
+        $this->resetVars();
+        $this->modelId = $id;
+        $this->modalFormVisible = true;
+        $this->loadModel();
+    }
+
+    /**
+     * loads the model model
+     * in this component
+     *
+     * @return void
+     */
+    public function loadModel()
+    {
+        $data = User::find($this->modelId);
+        $this->name = $data->name;
+        $this->email = $data->email;
+    }
+
+    /**
+     * the data for de model mapped
+     * in this component
+     *
+     * @return void
+     */
+    public function modelData()
+    {
+        $user =  [
+            'name' => $this->name,
+            'email' => $this->email,
+        ];
+
+        if ($this->password != null) {
+            $user['password'] = $this->password;
+        }
+
+        return $user;
+    }
+
+    /**
+     * Resets all the cariables
+     * to null
+     *
+     * @return void
+     */
+    public function resetVars()
+    {
+        $this->modelId = null;
+        $this->name = null;
+        $this->email = null;
+        $this->password = null;
+    }
+
+
+
+    /**
+     * The render function
+     *
+     * @return void
+     */
     public function render()
     {
         return view('livewire.index-users', [
--- resources/views/livewire/index-users.blade.php
+++ resources/views/livewire/index-users.blade.php
@@ -1,12 +1,17 @@
 <div>
-    <x-slot name="header">
-        <h2 class="flex items-baseline justify-between font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-            <x-jet-button wire:click="CreateShowModal">
-                {{ __('Create') }}
-            </x-jet-button>
-        </h2>
-    </x-slot>
+    <div class="bg-white shadow">
+        <div class="flex items-baseline justify-between max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class=" font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Admin/Users') }}
+            </h2>
+            <div class="mr-2">
+                <x-jet-button wire:click="CreateShowModal" class="bg-blue-500 hover:bg-blue-700">
+                    <i class="fas fa-plus"></i>&nbsp; {{ __('Create user') }}
+                </x-jet-button>
+            </div>
+        </div>
+    </div>
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
@@ -44,13 +49,15 @@
                                         <td class="px-6 py-4 whitespace-nowrap">
                                             {{ $user->email }}
                                         </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <x-jet-button wire:click="updateShowModal({{ $user->id }})">
-                                                {{ __('Update') }}
+                                        <td class="px-4 py-2 whitespace-nowrap text-right text-sm font-medium">
+                                            <x-jet-button wire:click="updateShowModal({{ $user->id }})"
+                                                class="bg-green-500 hover:bg-green-700">
+                                                <i class="fas fa-edit"></i>
                                             </x-jet-button>
-                                            <x-jet-danger-button wire:click="deleteShowModal({{ $user->id }})">
-                                                {{ __('Delete') }}
-                                            </x-jet-danger-button>
+                                            <x-jet-button wire:click="deleteShowModal({{ $user->id }})"
+                                                class="bg-red-500 hover:bg-red-700">
+                                                <i class="fas fa-trash"></i>
+                                            </x-jet-button>
                                         </td>
                                     </tr>
                                 @endforeach
@@ -70,3 +77,54 @@
             </div>
         </div>
     </div>
+
+    {{-- modal form --}}
+    <x-jet-dialog-modal wire:model="modalFormVisible">
+        <x-slot name="title">
+            {{ __('Save User') }}
+        </x-slot>
+
+        <x-slot name="content">
+            <div class="mt-4">
+                <x-jet-label for="name" value="{{ __('Name') }}" />
+                <x-jet-input id="name" class="block mt-1 w-full" type="text" wire:model.debounce.80ms="name" />
+                <x-jet-input-error for="name" />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="email" value="{{ __('Email') }}" />
+                <x-jet-input id="email" class="block mt-1 w-full" type="email" wire:model.debounce.80ms="email" />
+                <x-jet-input-error for="email" />
+            </div>
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Password') }}" />
+                <x-jet-input id="password" class="block mt-1 w-full" type="password"
+                    wire:model.debounce.80ms="password" />
+                <x-jet-input-error for="password" />
+            </div>
+
+
+            <div class="mt-4">
+                <x-jet-label for="password" value="{{ __('Confirm Password') }}" />
+                <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    wire:model.model.debounce.80ms="password_confirmation" />
+            </div>
+        </x-slot>
+
+        <x-slot name="footer">
+            <x-jet-secondary-button wire:click="$toggle('modalFormVisible')" wire:loading.attr="disabled">
+                {{ __('Nevermind') }}
+            </x-jet-secondary-button>
+
+            @if ($modelId)
+                <x-jet-button class="ml-2" wire:click="update" wire:loading.attr="disabled">
+                    {{ __('Update') }}
+                </x-jet-button>
+            @else
+                <x-jet-button class="ml-2" wire:click="create" wire:loading.attr="disabled">
+                    {{ __('Create') }}
+                </x-jet-button>
+            @endif
+        </x-slot>
+    </x-jet-dialog-modal>
+
+</div>
